ea1f38bc3aed5c042a91ce767276d6ff4a89cd0f
mannan90 <furball_wolfi@hotmail.com>
2014-06-07 13:28:04 +0500

    test changes 7

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
  *
  * @var bool
  */
-/* changes for branch 2 with tag1.0*/
+/* changes for branch 2 with tag1.1*/
 define('WP_USE_THEMES', true);
 
 /** Loads the WordPress Environment and Template */
